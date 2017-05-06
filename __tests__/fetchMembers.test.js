01a7244aa06a7b80d6316ba1cc48dd1b396747c8
hidace <dacewillmott@gmail.com>
2017-05-05 23:04:44 -0400

    add tests for members endpoints

---
+++ __tests__/fetchMembers.test.js
@@ -0,0 +1,14 @@
+import 'react-native';
+import React from 'react';
+import fetchData from '../src/utils/api/fetch-data';
+import endpoints from '../src/utils/api/config/endpoints';
+
+it('expects house members endpoint to be valid', () => {
+  expect(endpoints.members.house)
+    .toEqual('https://api.propublica.org/congress/v1/115/house/members.json');
+});
+
+it('expects senate members endpoint to be valid', () => {
+  expect(endpoints.members.senate)
+    .toEqual('https://api.propublica.org/congress/v1/115/senate/members.json');
+});
