486cf1ca24e530844a98749cafd4abdceffbbc5f
Dace Willmott <dacewillmott@gmail.com>
2017-05-07 17:56:34 -0400

    WIP: configure statesList component to show list of states

---
+++ src/components/StatesList/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const StatesList = () => {
+  return (
+    <View>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor aliquid est eligendi ipsam corporis, minus voluptate eaque itaque excepturi ullam sunt modi nam deleniti dolore dicta, officia voluptatem laboriosam hic!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor aliquid est eligendi ipsam corporis, minus voluptate eaque itaque excepturi ullam sunt modi nam deleniti dolore dicta, officia voluptatem laboriosam hic!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor aliquid est eligendi ipsam corporis, minus voluptate eaque itaque excepturi ullam sunt modi nam deleniti dolore dicta, officia voluptatem laboriosam hic!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor aliquid est eligendi ipsam corporis, minus voluptate eaque itaque excepturi ullam sunt modi nam deleniti dolore dicta, officia voluptatem laboriosam hic!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor aliquid est eligendi ipsam corporis, minus voluptate eaque itaque excepturi ullam sunt modi nam deleniti dolore dicta, officia voluptatem laboriosam hic!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor aliquid est eligendi ipsam corporis, minus voluptate eaque itaque excepturi ullam sunt modi nam deleniti dolore dicta, officia voluptatem laboriosam hic!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor aliquid est eligendi ipsam corporis, minus voluptate eaque itaque excepturi ullam sunt modi nam deleniti dolore dicta, officia voluptatem laboriosam hic!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor aliquid est eligendi ipsam corporis, minus voluptate eaque itaque excepturi ullam sunt modi nam deleniti dolore dicta, officia voluptatem laboriosam hic!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor aliquid est eligendi ipsam corporis, minus voluptate eaque itaque excepturi ullam sunt modi nam deleniti dolore dicta, officia voluptatem laboriosam hic!
+      </Text>
+    </View>
+  )
+};
+
+export default StatesList;
