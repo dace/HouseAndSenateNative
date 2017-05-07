0f0cc8ebd07b7ac58b3db99b87bb361f65cc297a
Dace Willmott <dacewillmott@gmail.com>
2017-05-07 17:55:19 -0400

    add actions to titles

--- src/components/MenuHome/index.js
+++ src/components/MenuHome/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { 
   View, 
-  StyleSheet 
+  StyleSheet,
 } from 'react-native';
 import MenuSection from '../MenuSection';
+import { Actions } from 'react-native-router-flux';
 
 const MenuHome = () => {
   return (
     <View style={styles.wrapper}>
-      <MenuSection title="Bills" />
-      <MenuSection title="Members" />
+      <MenuSection title="Bills" action={Actions.menuSelectChamber} />
+      <MenuSection title="Members" action={Actions.statesList} />
     </View>
   );
 };
